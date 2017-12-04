95701c290a2af6312a033ce56d3b1233096c69db
Mark Bello <mcbello51286@gmail.com>
2017-12-04 19:18:46 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,6 +2,6 @@ require "pry"
 class Hash
 
   def keys_of(*arguments)
-    map{|key, value| arguments.include?(value) ? key : nil}
+    map{|key, value| arguments.include?(value) ? key : nil}.compact
   end
 end
