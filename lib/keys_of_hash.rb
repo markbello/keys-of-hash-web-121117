b9b7fe7feb030cab84f38478dd96257c49e2d664
Mark Bello <mcbello51286@gmail.com>
2017-12-04 19:02:22 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,8 +3,7 @@ class Hash
   def keys_of(*arguments)
     key_array = []
     arguments.each{|key, value|
-      key_array.push(value)
-      binding.pry
+      key_array.push(key)
     }
     key_array
   end
